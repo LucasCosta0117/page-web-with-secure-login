440efd033772b23ed885cf274a6b07d0a7df50ad
Lucas Costa <lucas.costa0117@gmail.com>
2023-04-14 08:53:08 -0300

    feat: adiciona o arquivo 'index.php' com a page padrão de login do projeto

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Home</title>
+</head>
+<body>
+    <h1>Bem vindo!</h1>
+    <p>Para continuar realize seu Login.</p>
+
+    <form method="post" action="">
+        <label for="email">Email: </label>
+        <input type="email" id="email" name="email" placeholder="email@exemple.com">
+        <label for="pass">Senha: </label>
+        <input type="password" id="pass" name="pass">
+        <button type="submit">Entrar</button>
+    </form>
+
+    <a href="">
+        <span class="linkcadastro">Criar cadastro</span>
+    </a>
+    <a href="">
+        <span class="linkcadastro">Esqueci a senha</span>
+    </a>
+</body>
+</html>
